10d537881d8b582a3180432c0cfd928f158cc5c9
Peter Hayes <peter.k.hayes@gmail.com>
2015-02-20 00:36:03 -0800

    fixed bugs and added logging

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var body    = require("body/json");
 var _       = require("lodash");
+var morgan  = require("morgan");
 
 module.exports = function(options) {
   var app = express();
-
+  app.use(morgan('combined'));
+  
   /*
     Options and configuration.
   */
@@ -38,25 +40,26 @@ module.exports = function(options) {
 
   function sendError(res, status) {
     var msg = Array.prototype.slice.call(arguments, 2).join(" ");
+    res.status(status).send({error: msg});
   }
 
   /*
     Middleware to error on invalid collections.
   */
 
-  app.use(function(req, res, next) {
+  function ensureCollection(req, res, next) {
     var collection = req.params.collection;
     if (!storage[collection]) {
       return sendError(res, 400, "Unknown collection:", collection);
     }
     next();
-  });
+  }
 
   /*
-    Middleware to fetch an item for member GET, PUT, and DELETE
+    Middleware to fetch an item.
   */
 
-  app.use(function(req, res, next) {
+  function fetchItem(req, res, next) {
     var collection = req.params.collection;
     var id = req.params.id;
     if (!id) {
@@ -65,79 +68,79 @@ module.exports = function(options) {
 
     var fetched = findById(collection, id);
     if (!fetched) {
-      return sendError(res, 404, "Document not found with identifier:", idField, "-", id);
+      return sendError(res, 404, "Document not found with identifier:", idField, "/", id);
     }
     req.fetched = fetched;
     next();
-  });
+  }
 
   /*
     Body-parsing middleware for POST and PUT.
   */
 
-  app.use(function(req, res, next) {
+  function parseBody(req, res, next) {
     if (req.method === "POST" || req.method === "PUT") {
       body(req, res, function(err, body) {
         if (err) {
-          return sendError(res, 500, "Body parsing error");
+          return sendError(res, 500, "Body parsing error:", (err.message || err));
         }
         req.body = body;
         next();
       });
+    } else {
+      next();
     }
-  });
+  }
 
-
-  /*
-    Collection GET: retrieve all members of a collection.
-  */
-
-  app.get("/:collection", function(req, res) {
-    var collection = req.params.collection;
-    return res.json(storage[collection]);
-  });
-
-  /*
-    Collection POST: add a member to a collection.
-  */
-
-  app.post("/:collection", function(req, res) {
-    var collection = req.params.collection;
-    var id = data[idField];
-    if (!id) {
-      return sendError(res, 400, "Identifier not provided:", idField);
-    }
-    
-    if (findById(collection, id)) {
-      return sendError(res, 400, "Duplicated identifier:", idField, "-", id);
-    }
-    
-    storage[collection].push(data);
-    return res.status(201).json(data);
-  });
-
-  app.get("/:collection/:id", function(req, res) {
-    return res.json(req.fetched);
-  });
-
-  app.put("/:collection/:id", function(req, res) {
-    if (req.body[idField]) {
-      return sendError(res, 400, "Cannot edit identifier:", idField);
-    }
-    _.extend(req.fetched, req.body);
-    return res.json(req.fetched);
-  });
-
-  app.delete("/:collection/:id", function(req, res) {
-    var collection = req.params.collection;
-    var id = data[idField];
-    storage[collection] = storage[collection].filter(function(item) {
-      return item[idField] != id;
+  app.route("/:collection")
+    .all(ensureCollection)
+    .get(function(req, res) {
+      var collection = req.params.collection;
+      return res.json(storage[collection]);
+    })
+    .post(parseBody)
+    .post(function(req, res) {
+      var collection = req.params.collection;
+      var id = req.body[idField];
+      if (!id) {
+        return sendError(res, 400, "Identifier not provided:", idField);
+      }
+      
+      if (findById(collection, id)) {
+        return sendError(res, 400, "Duplicated identifier:", idField, "/", id);
+      }
+      
+      storage[collection].push(req.body);
+      return res.status(201).json(req.body);
     });
-    return res.status(204).send();
-  });
 
-  app.listen(port, function() {
+  app.route("/:collection/:id")
+    .all(ensureCollection)
+    .all(fetchItem)
+    .put(parseBody)
+    .get(function(req, res) {
+      return res.json(req.fetched);
+    })
+    .put(function(req, res) {
+      delete req.body[idField];
+      _.extend(req.fetched, req.body);
+      return res.json(req.fetched);
+    })
+    .delete(function(req, res) {
+      var collection = req.params.collection;
+      var id = req.params.id;
+      storage[collection] = storage[collection].filter(function(item) {
+        return item[idField] != id;
+      });
+      return res.status(204).send();
+    });
+
+  var server = app.listen(port, function() {
     console.log("Basic server listening on port", port);
   });
 };
+
+module.exports({
+  port: 3001,
+  collections: ["users", "playlists"]
+});
